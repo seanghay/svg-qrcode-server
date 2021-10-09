8fabc96354ce3f277178708e40f4c39963097f3a
Yath Seanghay <seanghay.dev@gmail.com>
2021-10-09 19:25:06 +0700

    add og

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import { promises as fs } from 'fs'
 import path from 'path'
+import Head from 'next/head'
+
 
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
 
 export default function App({ readmeContent }) {
-  return <ReactMarkdown rehypePlugins={[rehypeRaw]} children={readmeContent}></ReactMarkdown>
+  return <>
+    <Head>
+    <title>Dynamic QR Code to static image URL builder</title>  
+    <meta property="og:title" content="Dynamic QR Code to static image URL builder" key="title" />
+    </Head>    
+    <ReactMarkdown rehypePlugins={[rehypeRaw]} children={readmeContent}></ReactMarkdown>
+  </>
 }
 
 export async function getStaticProps() {
