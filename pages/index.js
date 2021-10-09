c28e471721ce420f999333ce987526d10e46c574
Yath Seanghay <seanghay.dev@gmail.com>
2021-10-09 18:47:38 +0700

    render index page from readme.md

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
-export default function App() {
-  return <h1>Static QR Code Server</h1>
+import { promises as fs } from 'fs'
+import path from 'path'
+import ReactMarkdown from 'react-markdown'
+import rehypeRaw from 'rehype-raw'
+
+export default function App({ readmeContent }) {
+  return <ReactMarkdown rehypePlugins={[rehypeRaw]} children={readmeContent}></ReactMarkdown>
+}
+
+export async function getServerSideProps() {
+  
+  const readmeFile = path.join(process.cwd(), 'README.md');
+  const readmeContent = await fs.readFile(readmeFile, 'utf-8');
+
+  return {
+    props: {
+      readmeContent,
+    }
+  }
 }
