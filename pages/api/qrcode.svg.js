89d29af2188f4c3aa1b6b67619f71dcaac57e6c3
seanghay <yathseanghay404@gmail.com>
2021-10-17 20:27:07 +0700

    Update qrcode.svg.js

--- pages/api/qrcode.svg.js
+++ pages/api/qrcode.svg.js
@@ -4,5 +4,6 @@ export default async (req, res) => {
   const text = req.query.text;
   const svg = new QRCode(text).svg();
   res.setHeader('Content-Type', 'image/svg+xml');
+  res.setHeader('Cache-Control', 'public, max-age=31536000, immutable');
   res.send(svg);
 }
